31b3ee1d0280685e34bfaa715791c42cece779a9
Vladi Mihtarski <132908375+VladiMihtarski@users.noreply.github.com>
2023-07-12 16:46:24 +0300

    Find_the_odd_int.py

---
+++ Find_the_odd_int.py
@@ -0,0 +1,18 @@
+# Given an array of integers, find the one that appears an odd number of times.
+# There will always be only one integer that appears an odd number of times.
+def find_it(seq):
+    element_count = {}
+    
+    # Преброяване на срещанията на всеки елемент в масива
+    for element in seq:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+    
+    # Намиране на елемента с нечетен брой срещания
+    for element, count in element_count.items():
+        if count % 2 != 0:
+            return element
+    
+    return None
